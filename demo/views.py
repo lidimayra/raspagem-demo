ce8160f524fbc7cd2e150eac9a34a1effa54232b
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-09-01 00:50:26 -0300

    Adiciona exemplo de busca por professores

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-# Create your views here.
 def index(request):
-    return HttpResponse('Hello')
+    query = request.GET.get('q', '')
+    teachers  = []
+
+    url = f'http://www.fatecjd.edu.br/portal/sobre-nos/corpo-docente/'
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read(), 'html.parser')
+    lines = soup.tbody.findAll('tr')
+
+    if (len(query)) > 0:
+        for line in lines:
+            name = line.td.text
+            if query.upper() in name:
+                teachers.append(line.text)
+
+    context_dict = { 'raw_values': teachers }
+    return render(request, 'demo/index.html', context_dict)

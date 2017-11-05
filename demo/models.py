c20fc3fca08e04f8f26180bbbebf65bcdc295f7b
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-11-05 15:48:26 -0200

    Adiciona representação do Edital
    Model CallNotice representa um Edital.

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CallNotice(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    url = models.TextField(unique=True)
--- demo/tests.py
+++ demo/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from .models import CallNotice
 
-# Create your tests here.
+class CallNoticeModelTests(TestCase):
+    def create_call_notice(self):
+        title = 'Concurso Artístico'
+        description = 'Lorem Ipsum'
+        url = 'http://www.exemplo.com.br'
+        call_notice = CallNotice.objects.create(title=title,
+                                                description=description,
+                                                url=url)
+        return call_notice
+
+    def test_creation(self):
+        call_notice = self.create_call_notice()
+        self.assertEqual(call_notice.title, 'Concurso Artístico')
+        self.assertEqual(call_notice.description, 'Lorem Ipsum')
+        self.assertEqual(call_notice.url, 'http://www.exemplo.com.br')
+
+    def test_url_uniqueness(self):
+        self.create_call_notice()
+
+        with self.assertRaises(IntegrityError):
+            self.create_call_notice()
